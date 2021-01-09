8cfbbc198813f983cc571a773acd3da07afb3405
Kozulina Nataliya <natasha.pechenka@gmail.com>
2021-01-09 13:55:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/* const result = confirm("Are you here?");
+console.log(result);
+ */
+
+/*  const answer = +prompt("How old are you?", "18");
+ console.log(answer+10); */
+
+/*  const answers = []; */
+
+/*  answers[0] = prompt('What is your name?', 'Bill');
+ answers[1] = prompt('What is your surname?', 'Smith');
+ answers[2] = prompt('How old are you?', '21'); */
+
+/* console.log(typeof(answers));
+document.write(answers); */
+
+/* const category = 'toys';
+
+console.log(`https://someurl/${category}/5`);
+const nameuser = +prompt('How old are you?', '16');
+alert(18 - nameuser); */
+
+let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
